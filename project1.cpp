a89f492bb8f2f89c09f371ab4fa2d587a301a46f
leonna <67146445+leonnahart@users.noreply.github.com>
2020-06-22 19:18:36 +0800

    add class Symptom

--- project1.cpp
+++ project1.cpp
@@ -1,6 +1,42 @@
 #include<iostream>
 
 using namespace std;
+class Symptom
+{
+	private:
+		std::string name;
+		float chance;
+		std::string question;
+	public:
+		Symptom()
+		{
+			name = "";
+			chance = 0.0;
+			question = "";
+			
+		}
+		void setName(std::string _name)
+		{
+			name = _name;
+		}
+		void setChance(float _chance)
+		{
+			chance = _chance;
+		}
+		std::string getName()
+		{
+			return name;
+		}
+		float getChance()
+		{
+			return chance;
+		}
+		void printQuestion()
+		{			  
+	        std::cout << "\nIn the last 14 days, do you:\n";
+            std::cout << "(please answer with 'yes' or 'no' only)\n\n";
+		}	
+};
 
 //main kosongin, diisi terakhir
 int main()
