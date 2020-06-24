2bab508aaab5b31c3d4a79306ae86e175ba8b41e
atifaaa <60084265+atifaaa@users.noreply.github.com>
2020-06-24 11:50:44 +0700

    Add class DiseaseHisotry

--- project1.cpp
+++ project1.cpp
@@ -3,6 +3,43 @@
 using namespace std;
 
 //tambahin kodemu mulai dibawah ini
+class DiseaseHistory
+{
+    private:
+        std::string name;
+        float chance;
+    public:
+        DiseaseHistory(std::string n = "")
+        {
+            name = n;
+        }
+        void setName (std::string disease)
+        {
+            name = disease;
+        }
+        void setChance (float ch)
+        {
+            chance = ch;
+        }
+        std::string getName()
+        {
+            return name;
+        }
+        float getChance()
+        {
+            return chance;
+        }
+        void printQuestion()
+        {
+			std::string ans;
+			std::cout << "(Answer Yes/No)";
+			std::cout << " Do you have any comorbid COVID-19 condition such as ";
+			std::cout << getName() << "\n";
+			std::cout << "Your Answer: ";
+			std::cin >> ans;			
+        }
+        
+};
 
 //main kosongin, diisi terakhir
 int main()
