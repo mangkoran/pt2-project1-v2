f5fa2079118242940fc9c7519ccb322924c98b10
ayams02 <57625126+ayams02@users.noreply.github.com>
2020-06-24 13:10:03 +0800

    Add Person class

--- project1.cpp
+++ project1.cpp
@@ -3,6 +3,49 @@
 using namespace std;
 
 //tambahin kodemu mulai dibawah ini
+class Symptom
+{
+private:
+    /* data */
+public:
+    Symptom(/* args */);
+};
+
+class State
+{
+private:
+    /* data */
+public:
+    State(/* args */);
+};
+
+class Person
+{
+private:
+    string name;
+    int age;
+    int count;
+    Symptom** symptom;
+    State** currLoc;
+    int chance;
+public:
+    Person(/* args */);
+    void setName()
+    {
+
+    }
+    void setAge()
+    {
+
+    }
+    void addSymptom(Symptom* s)
+    {
+        Symptom** temp = new Symptom*;
+
+    }
+};
+
+
 
 //main kosongin, diisi terakhir
 int main()
