d76787d486332e265cd90de1affc1cdbfa7675eb
ayams02 <57625126+ayams02@users.noreply.github.com>
2020-06-25 13:33:54 +0800

    Add method in Person

--- project1.cpp
+++ project1.cpp
@@ -21,27 +21,68 @@ public:
 
 class Person
 {
-private:
+protected:
     string name;
     int age;
     int count;
     Symptom** symptom;
-    State** currLoc;
+    State* currLoc;
     int chance;
 public:
-    Person(/* args */);
-    void setName()
+    Person()
     {
+        name = "";
+        age = 0;
+        count = 0;
+        symptom = NULL;
+        currLoc = NULL;
+        chance = 0;
 
+    };
+    void setName(string n)
+    {
+        name = n;
     }
-    void setAge()
+    void setAge(int a)
     {
-
+        age = a;
     }
     void addSymptom(Symptom* s)
     {
-        Symptom** temp = new Symptom*;
+        ++count;
+        Symptom** temp = new Symptom*[count];   //pointer to pointer to class
+        for(int i = 0; i < count-1; ++i)        //if count == 1 this loop is ignored
+        {
+            temp[i] = symptom[i];               //assign symptom element into temp element
+        }
+        temp[count-1] = s;                      //assign s into last temp element
+        symptom = temp;                         //assign temp into symptom
+        if(count == 1) delete temp;             //delete temp memory
+        else delete[] temp;
 
+
+    }
+    void setCurrLoc(State* s)
+    {
+        currLoc = s;
+    }
+    virtual void setChance()
+    {
+        // if(count != 0)                          //if no symmptom, chance = 0
+        // {
+        //     for(int i = 0; i < count; ++i)
+        //     {
+        //         // chance += symptom[i]->getChance();
+        //     }
+        // }
+    }
+    string getName()
+    {
+        return name;
+    }
+    int getChance()
+    {
+        return chance;
     }
 };
 
