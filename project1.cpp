4f1e0ca5fce57084057c4e6d07e29239a50a2662
ayams02 <57625126+ayams02@users.noreply.github.com>
2020-06-27 16:00:50 +0800

    Change dynamic array to vector

--- project1.cpp
+++ project1.cpp
@@ -1,94 +1,95 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<vector>
 using namespace std;
 
 //tambahin kodemu mulai dibawah ini
 class Symptom
 {
-	private:
-		string name;
-		float chance;
-		string question;
-	public:
-		Symptom()
-		{
-			name = "";
-			chance = 0.0;
-			question = "";
-			
-		}
-		void setName(string _name)
-		{
-			name = _name;
-		}
-		void setChance(float _chance)
-		{
-			chance = _chance;
-		}
-		string getName()
-		{
-			return name;
-		}
-		float getChance()
-		{
-			return chance;
-		}
-		void printQuestion()
-		{			  
-	        cout << "\nIn the last 14 days, do you:\n";
-            cout << "(please answer with 'yes' or 'no' only)\n\n";
-		}	
+private:
+    string name;
+    float chance;
+    string question;
+public:
+    Symptom()
+    {
+        name = "";
+        chance = 0.0;
+        question = "";
+        
+    }
+    void setName(string _name)
+    {
+        name = _name;
+    }
+    void setChance(float _chance)
+    {
+        chance = _chance;
+    }
+    string getName()
+    {
+        return name;
+    }
+    float getChance()
+    {
+        return chance;
+    }
+    void printQuestion()
+    {			  
+        cout << "\nIn the last 14 days, do you:\n";
+        cout << "(please answer with 'yes' or 'no' only)\n\n";
+    }	
 };
 
 class State
 {
-    private:
-		string name;
-		string status;
-		float chance;
-	public:
-		State()
-		{
-			name = "";
-			status = "";
-			chance = 0.0;
-		}
-		void setName(string n)
-		{
-			name = n;
-		}
-		void setStatus(string _status)
-		{
-			status = _status;
-		}
-		void setChance(float c)
-		{
-			chance = c;
-		}
-		string getName()
+private:
+    string name;
+    string status;
+    float chance;
+public:
+    State()
+    {
+        name = "";
+        status = "";
+        chance = 0.0;
+    }
+    void setName(string n)
+    {
+        name = n;
+    }
+    void setStatus(string _status)
+    {
+        status = _status;
+    }
+    void setChance(float c)
+    {
+        chance = c;
+    }
+    string getName()
+    {
+        return name;
+    }
+    string getStatus()
+    {
+        return status;
+    }
+    float getChance()
+    {
+        return chance;
+    }
+    void readFile()
+    {
+        ifstream fin ("statezone.txt");
+        int count = 0;
+        while ( getline(fin,name,',') )
         {
-            return name;
+            getline(fin,status);
+            count++;
         }
-		string getStatus()
-        {
-            return status;
-        }
-		float getChance()
-        {
-            return chance;
-        }
-        void readFile()
-        {
-            ifstream fin ("statezone.txt");
-			int count = 0;
-			while ( getline(fin,name,',') )
-			{
-				getline(fin,status);
-				count++;
-			}
-			fin.close();
-        }
+        fin.close();
+    }
 };
 
 class Person
@@ -97,7 +98,7 @@ protected:
     string name;
     int age;
     int count;
-    Symptom** symptom;
+    vector<Symptom*> symptom;
     State* currLoc;
     int chance;
 public:
@@ -106,10 +107,7 @@ public:
         name = "";
         age = 0;
         count = 0;
-        symptom = NULL;
-        currLoc = NULL;
         chance = 0;
-
     };
     void setName(string n)
     {
@@ -121,18 +119,7 @@ public:
     }
     void addSymptom(Symptom* s)
     {
-        ++count;
-        Symptom** temp = new Symptom*[count];   //pointer to pointer to class
-        for(int i = 0; i < count-1; ++i)        //if count == 1 this loop is ignored
-        {
-            temp[i] = symptom[i];               //assign symptom element into temp element
-        }
-        temp[count-1] = s;                      //assign s into last temp element
-        symptom = temp;                         //assign temp into symptom
-        if(count == 1) delete temp;             //delete temp memory
-        else delete[] temp;
-
-
+        symptom.push_back(s);
     }
     void setCurrLoc(State* s)
     {
@@ -140,13 +127,10 @@ public:
     }
     virtual void setChance()
     {
-        // if(count != 0)                          //count == 0 -> no symptom
-        // {
-        //     for(int i = 0; i < count; ++i)
-        //     {
-        //         // chance += symptom[i]->getChance();
-        //     }
-        // }
+        if(count != 0)                          //count == 0 -> no symptom
+        {
+            for(auto i : symptom) chance += i->getChance();
+        }
     }
     string getName()
     {
@@ -175,13 +159,10 @@ public:
     {
         if(count != 0)
         {
-            for(int i = 0; i < count; ++i)
-            {
-                // chance += symptom[i]->getChance();
-            }
+            for(auto i : symptom) chance += i->getChance();
         }
-        // chance += currLoc->getChance();
-        // chance += prevLoc->getChance();
+        chance += currLoc->getChance();
+        chance += prevLoc->getChance();
     }
 };
 
@@ -195,52 +176,39 @@ public:
     {
         if(count != 0)                          //count == 0 -> no symptom
         {
-            for(int i = 0; i < count; ++i)
-            {
-                // chance += symptom[i]->getChance();
-            }
+            for(auto i : symptom)  chance += i->getChance();
         }
-        // chance += currLoc->getChance();
-        // chance += prevLoc->getChance();
+        chance += currLoc->getChance();
         chance += 5;
     }
 };
 
 class DiseaseHistory
 {
-    private:
-        string name;
-        float chance;
-    public:
-        DiseaseHistory(string n = "")
-        {
-            name = n;
-        }
-        void setName (string disease)
-        {
-            name = disease;
-        }
-        void setChance (float ch)
-        {
-            chance = ch;
-        }
-        string getName()
-        {
-            return name;
-        }
-        float getChance()
-        {
-            return chance;
-        }
-        void printQuestion()
-        {
-			string ans;
-			cout << "(Answer Yes/No)";
-			cout << " Do you have any comorbid COVID-19 condition such as ";
-			cout << getName() << "\n";
-			cout << "Your Answer: ";
-			cin >> ans;			
-        }
+private:
+    string name;
+    int chance;
+public:
+    DiseaseHistory()
+    {
+        name = "";
+    }
+    void setName (string disease)
+    {
+        name = disease;
+    }
+    void setChance (int c)
+    {
+        chance = c;
+    }
+    string getName()
+    {
+        return name;
+    }
+    int getChance()
+    {
+        return chance;
+    }
 };
 
 void intro0()
@@ -258,6 +226,8 @@ void intro0()
 //main kosongin, diisi terakhir
 int main()
 {
+    vector<Symptom> vec;
+    intro0();
     cout << "\n";
     system("pause");
     return 0;
